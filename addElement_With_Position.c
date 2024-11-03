8231bafe6fe5f58a3c3be0ec782fb6a67f1d4bd2
Aashish <aashishverma239@gmail.com>
2024-11-03 23:29:20 +0530

    add element in array with position

---
+++ addElement_With_Position.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a[5],n, i, ele, pos;
+    
+    printf("Enter the size of an array 1-100: " );
+    scanf("%d",&n);
+    
+    printf("Enter %d integer: ",n);
+    for(i=0; i<n; i++){
+        scanf("%d",&a[i]);
+    }
+    
+    printf("Enter the element and postion: ");
+    scanf("%d %d",&ele,&pos);
+    
+    if(pos<1||pos>n+1){
+        printf("position 1 to %d only\n",n+1);
+        printf("Enter the element and postion: ");
+        scanf("%d %d",&ele,&pos);
+    }
+    
+    for(i=n; i>=pos; i--){
+        a[i]=a[i-1];
+    }
+   
+    a[i] = ele;
+    
+    printf("Elements: ");
+    for(i=0; i<=n; i++){
+        printf("%d ",a[i]);
+    }
+
+
+    return 0;
+}
